c381dca2d5017ef33e72dbde5bfb44b432131d88
jassem-1 <soueyjassem@gmail.com>
2025-01-28 13:14:20 +0100

    updates resume

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -20,22 +20,30 @@ const Hero = () => {
     const phoneNumber = "+216 55392530";
 
     if (isMobile) {
-        // For mobile devices, initiate a call
-        window.location.href = `tel:${phoneNumber}`;
+      // For mobile devices, initiate a call
+      window.location.href = `tel:${phoneNumber}`;
     } else {
-        // For desktop devices, open WhatsApp
-        window.open(`https://wa.me/${phoneNumber.replace(/[^0-9]/g, '')}`, '_blank');
+      // For desktop devices, open WhatsApp
+      window.open(
+        `https://wa.me/${phoneNumber.replace(/[^0-9]/g, "")}`,
+        "_blank"
+      );
     }
-};
+  };
   const lottieRef = useRef();
 
   return (
-    <section id="up" className="hero flex flex-col items-center min-h-screen pt-4">
+    <section
+      id="up"
+      className="hero flex flex-col items-center min-h-screen pt-4"
+    >
       <Navbar />
 
       <div id="up" className="flex pt-24 z-40 pl-4">
         <div className="left-section flex flex-col gap-6 items-start justify-start pt-8 ">
-          <div className="flex items-end gap-6 mb-8  relative"> {/* Make this relative */}
+          <div className="flex items-end gap-6 mb-8  relative">
+            {" "}
+            {/* Make this relative */}
             <Lottie
               lottieRef={lottieRef}
               animationData={circleAnimation} // Use the imported variable here
@@ -61,50 +69,49 @@ const Hero = () => {
           />
 
           <p className="sub-title text-base sm:text-xl md:text-2xl">
-            I'm <span className="text-gray-100 font-semibold">Jassem Souey</span> , a passionate fullstack developer specialized in building scalable,
+            I'm{" "}
+            <span className="text-gray-100 font-semibold">Jassem Souey</span> ,
+            a passionate fullstack developer specialized in building scalable,
             user-centric applications, with a focus on frontend development.
           </p>
           <div className="all-icons flex">
-  {/* GitHub Icon */}
-  <a
-    href="https://github.com/jassem-1" // Replace with your GitHub URL
-    target="_blank" // Opens link in a new tab
-    rel="noopener noreferrer" // Security measure for external links
-  >
-    <div className="icon-github"></div>
-  </a>
+            {/* GitHub Icon */}
+            <a
+              href="https://github.com/jassem-1" // Replace with your GitHub URL
+              target="_blank" // Opens link in a new tab
+              rel="noopener noreferrer" // Security measure for external links
+            >
+              <div className="icon-github"></div>
+            </a>
 
-  {/* LinkedIn Icon */}
-  <a
-    href="https://www.linkedin.com/in/jassem-souey-16b951278/" // Replace with your LinkedIn URL
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <div className="icon-linkedin"></div>
-  </a>
-  <a
-    href="https://www.facebook.com/jassem.souey123/" // Replace with your LinkedIn URL
-    target="_blank"
-    rel="noopener noreferrer"
-    
-  > <FaFacebook />
-</a>
-
-
-</div>
-<div className="border border-gray-600 p-2 rounded-full mt-3">
-<a
-        href="#"
-        onClick={handleClick}
-        className="flex gap-2 text-gray-400 items-center border border-gray-200"
-    >
-        <FaPhoneAlt />
-        <span className="text-xl">+216 55392530</span>
-        <FaWhatsapp className="text-xl"/>
-
-    </a>
-</div>
-
+            {/* LinkedIn Icon */}
+            <a
+              href="https://www.linkedin.com/in/jassem-souey-16b951278/" // Replace with your LinkedIn URL
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="icon-linkedin"></div>
+            </a>
+            <a
+              href="https://www.facebook.com/jassem.souey123/" // Replace with your LinkedIn URL
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              <FaFacebook />
+            </a>
+          </div>
+          <div className="border border-gray-600 p-2 rounded-full mt-3">
+            <a
+              href="#"
+              onClick={handleClick}
+              className="flex gap-2 text-gray-400 items-center border border-gray-200"
+            >
+              <FaPhoneAlt />
+              <span className="text-xl">+216 55392530</span>
+              <FaWhatsapp className="text-xl" />
+            </a>
+          </div>
         </div>
 
         <div className="right-section animation ">
@@ -121,9 +128,9 @@ const Hero = () => {
         </div>
       </div>
 
-      <a 
+      <a
         className="mt-2"
-        href="/cv-developer.pdf" // Path to the PDF file
+        href="/cv-pfe.pdf" // Path to the PDF file
         download="jassem-resume.pdf" // This triggers the file download with a custom file name
       >
         <NeonButton />
@@ -133,4 +140,4 @@ const Hero = () => {
   );
 };
 
-export default Hero;  
+export default Hero;
